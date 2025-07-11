3cf0f1b46e4f79bdf54baf7320f4a50b5e9baa0e
Bichngocc07 <ngoc01072006@gmail.com>
2025-07-11 19:40:44 +0700

    Update sesex001.c

--- Sess16/sesex001.c
+++ Sess16/sesex001.c
@@ -1,3 +1,39 @@
 //
 // Created by Owner on 7/11/2025.
 //
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+typedef struct Queue {
+    Node* front;
+    Node* rear;
+} Queue;
+
+Queue* createQueue() {
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    if (q != NULL) {
+        q->front = NULL;
+        q->rear = NULL;
+    }
+    return q;
+}
+
+int isEmpty(Queue* q) {
+    return (q->front == NULL);
+}
+
+int main() {
+    Queue* q = createQueue();
+    if (q != NULL && isEmpty(q)) {
+        printf("Hang doi da duoc khoi tao thanh cong va dang rong.\n");
+    } else {
+        printf("Co loi khi khoi tao hang doi.\n");
+    }
+    free(q);
+    return 0;
+}
