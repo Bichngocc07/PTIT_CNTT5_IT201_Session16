c91dff9dcf29b8d4fa32b39b46e1c9721b4ea684
Bichngocc07 <ngoc01072006@gmail.com>
2025-07-11 19:42:47 +0700

    Update sesex003.c

--- Sess16/sesex003.c
+++ Sess16/sesex003.c
@@ -1,3 +1,74 @@
 //
 // Created by Owner on 7/11/2025.
 //
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+typedef struct Queue {
+    Node* front;
+    Node* rear;
+} Queue;
+
+Queue* createQueue() {
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    if (q != NULL) {
+        q->front = NULL;
+        q->rear = NULL;
+    }
+    return q;
+}
+
+int isEmpty(Queue* q) {
+    return (q->front == NULL);
+}
+
+Node* createNode() {
+    int x;
+    printf("Nhap so nguyen: ");
+    scanf("%d", &x);
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode != NULL) {
+        newNode->data = x;
+        newNode->next = NULL;
+    }
+    return newNode;
+}
+
+void enqueue(Queue* q) {
+    Node* newNode = createNode();
+    if (newNode == NULL) return;
+    if (q->rear == NULL) {
+        q->front = q->rear = newNode;
+    } else {
+        q->rear->next = newNode;
+        q->rear = newNode;
+    }
+}
+
+void printQueue(Queue* q) {
+    Node* temp = q->front;
+    printf("Queue: front->");
+    while (temp != NULL) {
+        printf("%d->", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void peekFront(Queue* q) {
+    if (isEmpty(q)) {
+        printf("Queue is empty\n");
+    } else {
+        printf("%d\n", q->front->data);
+    }
+}
+
+int main() {
+    Queue* q = createQueue();
+    peekFront(q);
+    enqueue(
